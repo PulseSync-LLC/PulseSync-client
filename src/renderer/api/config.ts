53a26a3363e24d60fb8224b7899f08c9bfa5e6e0
EvT <one.evt@gmail.com>
2024-11-18 07:35:55 +0300

    tss

--- src/renderer/api/config.ts
+++ src/renderer/api/config.ts
@@ -1,9 +1,9 @@
 export const isDev = true
 
 export default {
-    SERVER_URL: isDev ? 'http://91.243.98.92:4000' : 'https://api.pulsesync.dev',
+    SERVER_URL: isDev ? 'http://localhost:4000' : 'https://api.pulsesync.dev',
     SOCKET_URL: isDev
-        ? 'http://91.243.98.92:1337/'
+        ? 'http://localhost:1337/'
         : 'https://socket.pulsesync.dev/',
     RETRY_INTERVAL_MS: 15000,
     MAX_RETRY_COUNT: 10,
