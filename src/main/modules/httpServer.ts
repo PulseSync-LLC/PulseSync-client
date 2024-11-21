ef5d8043ee8abb204df0290ae794a8657ba9209b
Anton Zlobin <anton.zzlobin121@gmail.com>
2024-11-21 04:56:21 +0200

    мсч

--- src/main/modules/httpServer.ts
+++ src/main/modules/httpServer.ts
@@ -83,68 +83,6 @@ server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
         return
     }
 
-    if (req.method === 'GET' && req.url === '/get_theme') {
-        try {
-            if (authorized || isAppDev) {
-                const themesPath = path.join(
-                    app.getPath('appData'),
-                    'PulseSync',
-                    'themes',
-                )
-                const themePath = path.join(themesPath, selectedTheme)
-                const metadataPath = path.join(themePath, 'metadata.json')
-
-                if (fs.existsSync(metadataPath)) {
-                    const metadata = JSON.parse(
-                        fs.readFileSync(metadataPath, 'utf8'),
-                    )
-                    let scriptJS = null
-                    let cssContent = ''
-                    let jsContent = ''
-                    const styleCSS = path.join(themePath, metadata.css)
-                    if (metadata.script) {
-                        scriptJS = path.join(themePath, metadata.script)
-                        if (fs.existsSync(scriptJS)) {
-                            jsContent = fs.readFileSync(scriptJS, 'utf8')
-                        }
-                    }
-
-                    if (fs.existsSync(styleCSS)) {
-                        cssContent = fs.readFileSync(styleCSS, 'utf8')
-                    }
-
-                    res.writeHead(200, { 'Content-Type': 'application/json' })
-                    res.end(
-                        JSON.stringify({
-                            ok: true,
-                            css: cssContent ? cssContent : '{}',
-                            script: jsContent ? jsContent : '',
-                        }),
-                    )
-                    return
-                }
-
-                res.writeHead(404, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify({ error: 'Metadata not found' }))
-            } else {
-                res.writeHead(200, { 'Content-Type': 'application/json' })
-                res.end(
-                    JSON.stringify({
-                        ok: true,
-                        css: '{}',
-                        script: '',
-                    }),
-                )
-                return
-            }
-        } catch (error) {
-            logger.http.error('Error reading theme files:', error)
-            res.writeHead(500, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ error: 'Error reading theme files' }))
-        }
-        return
-    }
-
     if (req.method === 'GET' && req.url === '/get_handle') {
         try {
             const handleEventsPath = path.join(
@@ -296,6 +234,7 @@ export const setTheme = (theme: string) => {
                 ok: true,
                 css: cssContent || '{}',
                 script: jsContent || '',
+                type: 'theme'
             }),
         ),
     )
