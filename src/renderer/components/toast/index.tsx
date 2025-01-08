a5951d343c72bfd445bed4c5bc631fd4388ea023
foreA-adoxid <forea.adoxid@gmail.com>
2025-01-08 19:29:45 +0300

    fix import
    Took 16 seconds

--- src/renderer/components/toast/index.tsx
+++ src/renderer/components/toast/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { JSX, useEffect, useState } from 'react'
 import toast, { Renderable, ToastOptions, Toast } from 'react-hot-toast'
 import * as styles from './toast.module.scss'
 
