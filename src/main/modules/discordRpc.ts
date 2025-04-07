245b2f9834d5d87210d1c9c7daaecd96af340ee3
foreA.adoxid <forea.adoxid@gmail.com>
2025-04-07 11:50:45 +0300

    fix check DiscordState on mac & linux

--- src/main/modules/discordRpc.ts
+++ src/main/modules/discordRpc.ts
@@ -8,18 +8,36 @@ import { mainWindow } from '../../index'
 import { updateTray } from './tray'
 import * as fs from 'node:fs'
 import * as net from 'net'
+import { promisify } from 'util'
+import { exec } from 'child_process'
 
 enum DiscordState {
     CLOSED = 'Не удалось обнаружить запущенный Discord!',
     ADMINISTRATOR = 'Похоже, Discord запущен с правами администратора. Запустите PulseSync с правами администратора.',
+    SNAP = "Похоже, Discord запущен из пакета Snap. Это, скорее всего, помешает приложению подключиться к RPC",
+    FLATPAK = "Похоже, Discord запущен из пакета Flatpak. Это, скорее всего, помешает приложению подключится к RPC",
     SUCCESS = '',
 }
+const execAsync = promisify(exec);
 const SET_ACTIVITY_TIMEOUT_MS = 1500
 let sendActivityTimeoutId: NodeJS.Timeout = undefined
 let previousActivity: SetActivity = undefined
 
 let reconnectTimeout: NodeJS.Timeout = undefined
 
+export async function readDiscord(): Promise<DiscordState> {
+    const platform = process.platform;
+
+    if (platform === 'win32') {
+        return await checkDiscordStateWin();
+    } else if (platform === 'linux') {
+        return await checkDiscordStateLinux();
+    } else if (platform === 'darwin') {
+        return await checkDiscordStateMac();
+    } else {
+        return DiscordState.CLOSED;
+    }
+}
 function removeTimestampsFromActivity(activity: any) {
     let copyActivity = JSON.parse(JSON.stringify(activity))
     if (copyActivity.startTimestamp) {
@@ -50,7 +68,56 @@ function compareActivities(newActivity: any) {
     )
 }
 
-async function checkDiscordState(): Promise<string> {
+export async function checkDiscordStateLinux(): Promise<DiscordState> {
+    try {
+        const { stdout } = await execAsync('ps xo user:30,command');
+        const lines = stdout
+            .split('\n')
+            .filter(line => line.toLowerCase().includes('/discord'))
+            .join('\n');
+
+        if (!lines.trim()) {
+            return DiscordState.CLOSED;
+        } else if (lines.toLowerCase().includes('/snap/discord')) {
+            return DiscordState.SNAP;
+        } else if (lines.toLowerCase().includes('/app/com.discordapp.discord')) {
+            return DiscordState.FLATPAK;
+        } else {
+            return DiscordState.SUCCESS;
+        }
+    } catch (error) {
+        logger.discordRpc.error('Error executing process command:', error);
+        return DiscordState.CLOSED;
+    }
+}
+
+export async function checkDiscordStateMac(): Promise<DiscordState> {
+    const clients = [
+        "Discord.app",
+        "Discord PTB.app",
+        "Discord Canary.app",
+        "Discord Development.app"
+    ];
+
+    try {
+        const { stdout } = await execAsync("ps -A");
+        const lines = stdout.split('\n');
+
+        const clientNotRunning = lines.every(line =>
+            !clients.some(client => line.includes(client))
+        );
+
+        if (clientNotRunning) {
+            return DiscordState.CLOSED;
+        }
+
+        return DiscordState.SUCCESS;
+    } catch (error) {
+        return DiscordState.CLOSED;
+    }
+}
+
+async function checkDiscordStateWin(): Promise<DiscordState> {
     const ipcPaths = Array.from({ length: 10 }, (_, i) => `\\\\.\\pipe\\discord-ipc-${i}`)
     let ipcPathFound: string | null = null
 
@@ -91,7 +158,7 @@ async function checkDiscordState(): Promise<string> {
  * для получения более точного описания проблемы.
  */
 async function handleRpcError(e: Error): Promise<string> {
-    const state = await checkDiscordState()
+    const state = await readDiscord()
     if (state !== DiscordState.SUCCESS) {
         return state
     }
@@ -208,7 +275,7 @@ async function rpc_connect() {
         clientId,
         transport: { type: 'ipc' },
     })
-    const discordState = await checkDiscordState()
+    const discordState = await readDiscord()
     if (discordState !== DiscordState.SUCCESS) {
         logger.discordRpc.error('Discord state error: ' + discordState)
         mainWindow.webContents.send('rpc-log', { message: discordState || 'Ошибка подключения к Discord RPC', type: 'error' })
