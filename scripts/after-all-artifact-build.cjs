0f44ee4e112114d7a56245fb5b21aa17542efdb1
foreA-adoxid <forea.adoxid@gmail.com>
2026-01-29 00:37:41 +0300

    fix: try to fix again

--- scripts/after-all-artifact-build.cjs
+++
@@ -1,5 +0,0 @@
-const { restoreFromBackup } = require('./license-encoding.cjs')
-
-module.exports = async () => {
-    restoreFromBackup()
-}
--- scripts/before-pack.cjs
+++
@@ -1,6 +0,0 @@
-const { convertToCp1251, restoreFromBackup } = require('./license-encoding.cjs')
-
-module.exports = async () => {
-    convertToCp1251()
-    process.once('exit', restoreFromBackup)
-}
