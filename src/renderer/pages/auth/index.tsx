c688d9ec5d9309665f3a63db1dd407bcdd05e783
Maks1mio <one.evt@gmail.com>
2026-02-07 21:42:02 +0300

    feat: enhance authentication page with default theme and space background

--- src/renderer/pages/auth/index.tsx
+++ src/renderer/pages/auth/index.tsx
@@ -1,10 +1,16 @@
 import { useMemo } from 'react'
 import AuthSummer from './summer/AuthSummer'
 import AuthWinter from './winter/AuthWinter'
+import AuthDefault from './default/Auth'
 import { getSeasonByMSK } from '../../utils/seasonDetector'
 
 export default function AuthPage() {
     const AUTH_THEME = useMemo(() => getSeasonByMSK(), [])
-
-    return AUTH_THEME === 'winter' ? <AuthWinter /> : <AuthSummer />
+    if (AUTH_THEME === 'summer') {
+        return <AuthSummer />
+    } else if (AUTH_THEME === 'winter') {
+        return <AuthWinter />
+    } else {
+        return <AuthDefault />
+    }
 }
