2df83beda08491fe54e69222a136e5af62abbab3
foreA-adoxid <forea.adoxid@gmail.com>
2026-06-29 23:34:37 +0300

    fix: avoid startup shell windows

--- src/main/utils/appUtils/powershell.ts
+++ src/main/utils/appUtils/powershell.ts
@@ -4,7 +4,7 @@ import { promisify } from 'util'
 const execFileAsync = promisify(execFile)
 
 export const runPowerShell = async (script: string, args: string[] = []): Promise<string> => {
-    const psArgs = ['-NoProfile', '-NonInteractive', '-Command', script, ...args]
+    const psArgs = ['-NoProfile', '-NonInteractive', '-WindowStyle', 'Hidden', '-Command', script, ...args]
     const { stdout } = (await execFileAsync('powershell.exe', psArgs, {
         windowsHide: true,
         timeout: 10000,
