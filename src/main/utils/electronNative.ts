a7a1050fada6bfc4b7def7f8224a5fca63fb9b2a
foreA-adoxid <forea.adoxid@gmail.com>
2026-07-12 01:10:30 +0300

    refactor: remove unused image fetcher

--- src/main/utils/electronNative.ts
+++ src/main/utils/electronNative.ts
@@ -1,15 +1,9 @@
-import { app, nativeImage, NativeImage } from 'electron'
+import { app, nativeImage } from 'electron'
 import path from 'path'
-import fs from 'node:fs'
 
-const getNativeImgFromUrl = async (url: string): Promise<NativeImage> => {
-    const res = await fetch(`https://${url.replaceAll('%%', '100x100')}`)
-
-    return nativeImage.createFromBuffer(Buffer.from(await res.arrayBuffer()))
-}
 const getNativeImg = (name: string, ext: string, useFor?: string) => {
     const basePath = app.isPackaged ? path.join(process.resourcesPath, 'assets') : path.join(app.getAppPath(), 'static', 'assets')
     const filePath = path.join(basePath, useFor ? useFor + '/' : '', `${name}${ext}`)
     return nativeImage.createFromPath(filePath)
 }
-export { getNativeImg, getNativeImgFromUrl }
+export { getNativeImg }
