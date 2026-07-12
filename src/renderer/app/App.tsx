46caf439c9f2ca3b3254569c36fc151ae4efabb4
foreA-adoxid <forea.adoxid@gmail.com>
2026-07-13 02:39:49 +0300

    feat: add browser download banner

--- src/renderer/app/App.tsx
+++ src/renderer/app/App.tsx
@@ -5,9 +5,14 @@ import client from '@shared/api/apolloClient'
 import { ApolloProvider } from '@apollo/client/react'
 import ModalContainer from '@widgets/modalContainer/ModalContainer'
 import { ModalProvider } from '@app/providers/modal'
+import BrowserDownloadBanner from '@widgets/browserDownloadBanner'
 import './i18n'
 
 function App() {
+    if (!window.pulsesyncDesktop) {
+        return <BrowserDownloadBanner />
+    }
+
     return (
         <ErrorBoundary>
             <ApolloProvider client={client}>
