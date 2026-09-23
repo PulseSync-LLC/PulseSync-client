18bed239d14b08cf082946fb909b2d10d171cd17
foreA-adoxid <forea.adoxid@gmail.com>
2026-09-24 01:05:00 +0300

    fix(addons): allow channel header on local file requests

--- src/main/modules/httpServer/httpRequestHandler.ts
+++ src/main/modules/httpServer/httpRequestHandler.ts
@@ -58,7 +58,7 @@ const setCorsHeaders = (req: http.IncomingMessage, res: http.ServerResponse, all
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-PulseSync-Channel',
     )
 }
 
